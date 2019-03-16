d98155af4b8f49f5a972e8f56b83ba8dae8188d0
Marcos Peña <devilcius@gmail.com>
2019-03-16 10:25:05 +0100

    Updates

---
+++ precache-manifest.041b410ece4565a5e59dde20c80c137e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f317303a788d0884858d4284ec10a066",
+    "url": "/react-spotify-album-player/static/media/audio_icons_set.f317303a.png"
+  },
+  {
+    "revision": "744597a75ba6b967279f",
+    "url": "/react-spotify-album-player/static/js/runtime~main.5004ec71.js"
+  },
+  {
+    "revision": "2e5811aa66de0631c02b",
+    "url": "/react-spotify-album-player/static/js/main.25c1e461.chunk.js"
+  },
+  {
+    "revision": "d11f9a41d706381a87d6",
+    "url": "/react-spotify-album-player/static/js/2.7afb739b.chunk.js"
+  },
+  {
+    "revision": "2e5811aa66de0631c02b",
+    "url": "/react-spotify-album-player/static/css/main.928b64f0.chunk.css"
+  },
+  {
+    "revision": "d11f9a41d706381a87d6",
+    "url": "/react-spotify-album-player/static/css/2.39d84c0e.chunk.css"
+  },
+  {
+    "revision": "289a88961c5677fa0e116eb687897e28",
+    "url": "/react-spotify-album-player/index.html"
+  }
+];
